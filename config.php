09f77c42e7e4a490d61d73872bc5d63ddef5ab47
PatW124 <35626640+PatW124@users.noreply.github.com>
2019-02-23 10:08:28 +0800

    update config

--- config.php
+++ config.php
@@ -5,9 +5,12 @@ $dbpassword = "BCxQf3xpYF";
 $dbname = "sql12280266";
 
 
-$connection = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+$connection = mysqli_connect($servername, $dbusername, $dbpassword);
 if (!$conn) {
     die("connection failed: ".mysqli_connect_error());
+ }
+else {
+    $db = mysql_select_db("$dbname", $connection);   
  }
 ?>
 
